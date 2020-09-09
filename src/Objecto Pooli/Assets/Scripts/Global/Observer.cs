16e2105d1bd15cb177ba0bf71210988baa3932b7
VectorArtGames <70201838+VectorArtGames@users.noreply.github.com>
2020-09-09 11:07:10 +0200

    initial upload

---
+++ src/Objecto Pooli/Assets/Scripts/Global/Observer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IObserver
+{
+	void OnNotify(Unit entity, EventType type);
+}
+
+public class Observer : MonoBehaviour, IObserver
+{
+	public virtual void OnNotify(Unit entity, EventType type)
+	{
+
+	}
+}
+
+
+public enum EventType
+{
+	Achievement
+}
---
+++ src/Objecto Pooli/Assets/Scripts/Spectator/Observatory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO Fix Observer
+
+//public class Observatory : MonoBehaviour
+//{
+//	public event 
+//}
+
+//public class NotifyEventArgs : EventArgs
+//{
+
+//}
